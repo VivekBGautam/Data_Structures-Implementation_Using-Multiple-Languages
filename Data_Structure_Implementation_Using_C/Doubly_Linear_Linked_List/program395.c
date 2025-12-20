3ee77ca6e9e0761ee558dfd4150246e6ab7e5367
Vivek Gautam <vivekbgautam@gmail.com>
2025-12-20 13:07:26 +0530

    Add files via upload

---
+++ Data_Structure_Implementation_Using_C/Doubly_Linear_Linked_List/program395.c
@@ -0,0 +1,172 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)                     // To Avoide padding
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;             // $
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+// Call By Value
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description   : Displays all elements of the singly linked list.
+//  Input         : PNODE first  →  Pointer to the first node of the linked list
+//  Output        : Prints all node data on screen
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 18/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void Display(PNODE head)
+{}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Count
+//  Description   : Counts the total number of nodes present in the linked list.
+//  Input         : PNODE first  →  Pointer to the first node of the linked list
+//  Output        : Returns integer count of nodes
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 18/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+int Count(PNODE first)
+{}
+
+// Call By Address
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : InsertFirst
+//  Description   : Inserts a new node at the beginning of the linked list.
+//  Input         : PPNODE first → Address of first pointer
+//                  int no        → Data to be inserted in the node
+//  Output        : Modifies linked list by adding new node at first position
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 18/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+    newn->prev = NULL;          //$
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        (*first)->prev = newn;          // $
+        *first = newn;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : InsertLast
+//  Description   : Inserts a new node at the end of the linked list.
+//  Input         : PPNODE first → Address of first pointer
+//                  int no        → Data to be inserted in the node
+//  Output        : Modifies linked list by adding new node at last position
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 18/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+    newn->prev = NULL;          // $
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : InsertAtPos
+//  Description   : Inserts a new node at a specific position in the linked list.
+//  Input         : PPNODE first → Address of first pointer
+//                  int no        → Data to be inserted
+//                  int pos       → Position where node is to be inserted
+//  Output        : Modifies linked list by placing new node at desired position
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 18/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void InsertAtPos(PPNODE first, int no, int pos)
+{}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DeleteFirst
+//  Description   : Deletes the first node of the linked list.
+//  Input         : PPNODE first → Address of first pointer
+//  Output        : Removes first node and frees its memory
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 18/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void DeleteFirst(PPNODE first)
+{}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DeleteLast
+//  Description   : Deletes the last node of the linked list.
+//  Input         : PPNODE first → Address of first pointer
+//  Output        : Removes last node and frees its memory
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 18/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void DeleteLast(PPNODE first)
+{}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DeleteAtPos
+//  Description   : Deletes a node from a specific position in the linked list.
+//  Input         : PPNODE first → Address of first pointer
+//                  int pos       → Position of the node to be deleted
+//  Output        : Removes node from given position and frees its memory
+//  Author        : Vivek Bhauraj Gautam
+//  Date          : 18/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void DeleteAtPos(PPNODE first, int pos)
+{}
+
+int main()
+{
+    PNODE head = NULL;
+
+
+    return 0;
+}
